0ffd7de0aae9578623b0489030bee448979bd68f
hasobi <roid.radityo11@gmail.com>
2020-04-12 13:56:57 +0700

    first commit

--- 7kyu/Square Every Digit/Square Every Digit.py
+++ 7kyu/Square Every Digit/Square Every Digit.py
@@ -1,12 +1,5 @@
 def square_digits(num):
-    res = list(str(num))
-    result = []
-    # iterasi dengan mengkuadratkan setiap anggota list
-    for i in res:
-        result.append(int(i)**2)
-
-    for i in result: 
-        print(i, end="") 
-
-
-square_digits(9119)
+    result = ''
+    for i in str(num):
+        result += str((int(i))**2)
+    return int(result) 
